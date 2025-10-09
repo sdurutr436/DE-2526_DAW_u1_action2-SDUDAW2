7502f286b3d47ebf00d4cbacd3ffde6c799c641b
sdurutr436 <sdurutr436@g.educaand.es>
2025-10-09 08:27:50 +0200

    Añadido directorio absoluto para que lo encuentre y dia y tiempo de ejecución de la prueba

--- update_reamde.py
+++ update_reamde.py
@@ -1,4 +1,7 @@
+import datetime
 import subprocess
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def run_tests():
     try:
@@ -8,6 +11,7 @@ def run_tests():
         return "❌ Tests fallidos"
 
 def update_readme(status: str):
+    execution_datetime = datetime.datetime.now().strftime(" -> %Y-%m-%d %H:%M:%S\n")
     with open("README.md", "r", encoding="utf-8") as f:
         lines = f.readlines()
 
@@ -15,7 +19,7 @@ def update_readme(status: str):
     for line in lines:
         new_lines.append(line)
         if line.strip() == "## Estado de los tests":
-            new_lines.append(status + "\n")
+            new_lines.append(status + execution_datetime + "\n")
 
     with open("README.md", "w", encoding="utf-8") as f:
         f.writelines(new_lines)
